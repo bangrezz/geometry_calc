cd617a6314538db600fc5a55f1cdfd6b486de99f
bangrezz <rezaekafebrian40@gmail.com>
2021-04-14 11:31:41 +0800

    Add new wannaquit modules

---
+++ modules/PlaneGeometry/Square/wannaquit.py
@@ -0,0 +1,34 @@
+from modules.clearscreen import clear
+from rich import print
+
+def MainQuestion():
+    print("""
+        ________________________________________________
+        What you want to do ?
+        
+        1. Count again
+        2. Go to the previous menu
+        99. Exit
+        """)
+    def question():
+        try:
+            select = int(input("Select : "))
+            if select == 1:
+                from modules.PlaneGeometry.Square.execute import Execute_Square
+                Execute_Square()
+            elif select == 2:
+                print("Press [blue]<enter>[/blue] to continue")
+                input()
+            elif select == 99:
+                print("\n\nThank you for using Geometry Calc :)")
+                exit()
+            else:
+                print("[red][ERROR][/red] Invalid input. Enter again !")
+                question()
+        except ValueError:
+            print("[red][ERROR][/red] Value error. Enter again !")
+            question()
+        except KeyboardInterrupt:
+            print("\n\nThank you for using Geometry Calc :)")
+            exit()
+    question()
